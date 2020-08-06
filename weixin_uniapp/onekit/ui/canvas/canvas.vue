d01a0373122d44f6075e2f4b5ebe4dc48d940ff8
hhm19990929 <65931506+hhm19990929@users.noreply.github.com>
2020-08-06 17:32:10 +0800

    canvas navigator

---
+++ weixin_uniapp/onekit/ui/canvas/canvas.vue
@@ -0,0 +1,35 @@
+<template>
+	<canvas :canvas-id="Id" :disable-scroll="disableScroll" :width="width" :height="height" @touchstart="bindtouchstart"></canvas>
+</template>
+
+<script>
+	export default {
+		props: {
+			Id: {
+				type: String,
+				value: '',
+			},
+			width: {
+				type: String,
+				value: '',
+			},
+			height: {
+				type: String,
+				value: '',
+			},
+			disableScroll: {
+				type: Boolean,
+				value: false
+			},
+		},
+		methods:{
+			bindtouchstart(e) {
+				console.log("bind start",e)
+				this.$emit("touchstart",e.details)
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
