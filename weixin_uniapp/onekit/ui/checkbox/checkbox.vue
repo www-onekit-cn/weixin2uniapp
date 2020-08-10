e8c915264190a791539e6a0f04a527762e793c59
gongyuan2 <939014677@qq.com>
2020-08-10 17:56:01 +0800

    1

--- weixin_uniapp/onekit/ui/checkbox/checkbox.vue
+++ weixin_uniapp/onekit/ui/checkbox/checkbox.vue
@@ -1,4 +1,5 @@
 <template>
+<<<<<<< HEAD
 	<checkbox
 		:value="value"
 		:disabled="disabled"
@@ -13,12 +14,29 @@
 <script>
 	export default{
 		props:{
+=======
+	<view>
+		<checkbox
+		:value = "value"
+		:disabled = "disabled"
+		:checked = "checked"
+		:color = "color"
+		>
+		</checkbox>
+	</view>
+</template>
+
+<script>
+	export default {
+		props: {
+>>>>>>> 20787aef25083209afeaa3e104b9a5af34e2d022
 			value:{
 				type:String,
 				default: ""
 			},
 			disabled:{
 				type:Boolean,
+<<<<<<< HEAD
 				default: false
 			},
 			checked:{
@@ -35,4 +53,19 @@
 </script>
 
 <style>
+=======
+				default:false
+			},
+			checked:[Boolean,String],
+			color:{
+				type:String,
+				default:"#09BB07"
+			}
+		}
+	}
+</script>
+
+<style>
+
+>>>>>>> 20787aef25083209afeaa3e104b9a5af34e2d022
 </style>
