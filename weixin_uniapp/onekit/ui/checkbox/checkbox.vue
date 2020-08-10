c060518ce7216c2a8920dd672a19817b2a239cc9
gongyuan2 <939014677@qq.com>
2020-08-10 17:52:37 +0800

    checkbox、button组件编写，camera、audio的bug修改

---
+++ weixin_uniapp/onekit/ui/checkbox/checkbox.vue
@@ -0,0 +1,38 @@
+<template>
+	<checkbox
+		:value="value"
+		:disabled="disabled"
+		:checked="checked"
+		:color="color"
+	
+	>
+		<slot/>
+	</checkbox>
+</template>
+
+<script>
+	export default{
+		props:{
+			value:{
+				type:String,
+				default: ""
+			},
+			disabled:{
+				type:Boolean,
+				default: false
+			},
+			checked:{
+				type:String,
+				default: "false"
+			},
+			color:{
+				type:String,
+				default: "#09BB07"
+			},
+		}
+	}
+	
+</script>
+
+<style>
+</style>
