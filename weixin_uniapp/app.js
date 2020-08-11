53778ff3ce466864ea66c84e750769a19d2e87c0
zhangjinva <zhangjin_va@qq.com>
2020-08-11 21:57:50 +0800

    Update app.js

--- weixin_uniapp/app.js
+++ weixin_uniapp/app.js
@@ -1,12 +1,3 @@
 import OnekitApp from "./onekit/OnekitApp.js"
 OnekitApp({
-		onLaunch: function() {
-			console.log('App Launch')
-		},
-		onShow: function() {
-			console.log('App Show')
-		},
-		onHide: function() {
-			console.log('App Hide')
-		}
 	})
