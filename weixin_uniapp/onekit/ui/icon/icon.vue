ee8c026c91f46ec00867a3fdd369a96aed55179d
liping <1850115720.com>
2020-08-11 15:04:08 +0800

    icon
    icon

---
+++ weixin_uniapp/onekit/ui/icon/icon.vue
@@ -0,0 +1,32 @@
+<template>
+	<view>
+		<icon
+			:type="type"
+			:size="size"
+			:color="color"
+		></icon>
+	</view>
+</template>
+
+<script>
+	export default {
+		props:{
+			type:{
+				type:String,
+				default: ""
+			},
+			size:{
+				type:Number/String,
+				default: 23/""
+			},
+			color:{
+				type:String,
+				default: ""
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
