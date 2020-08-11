613adc25e9e8a7a081c6fd34a5f77b697f6a3930
liping <1850115720.com>
2020-08-11 11:14:33 +0800

    switch
    switch

---
+++ weixin_uniapp/onekit/ui/switch/switch.vue
@@ -0,0 +1,46 @@
+<template>
+	
+		<switch
+			:checked="checked"
+			:disabled="disabled"
+			:type="type"
+			@change="switch_change"
+			:color="color"
+		>
+		<slot/>
+		</switch>
+
+</template>
+
+<script>
+	export default {
+			props:{
+				checked:{
+					type:Boolean,
+					default: false
+				},
+				disabled:{
+					type: Boolean,
+					default: false
+				},
+				type:{
+					type: String,
+					default: "switch"
+				},
+				color:{
+					type: String,
+					default: "#04BE02"
+				}
+			},
+		methods: {
+		  	switch_change(e){
+		  	        console.log("switch change success",e)
+		  	        this.$emit('change',e.details)
+		  	}, 
+		}
+	}
+</script>
+
+<style>
+
+</style>
