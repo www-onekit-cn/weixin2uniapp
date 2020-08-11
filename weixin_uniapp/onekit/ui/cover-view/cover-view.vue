d962c3135bda05f7c5781d8f370a724291bb371e
onekit-qing <64195283+onekit-qing@users.noreply.github.com>
2020-08-11 14:44:29 +0800

    .

---
+++ weixin_uniapp/onekit/ui/cover-view/cover-view.vue
@@ -0,0 +1,20 @@
+<template>
+	<cover-view :scroll-top="scrollTop">
+		<slot></slot>
+	</cover-view>
+</template>
+
+<script>
+	export default {
+		props: {
+			scrollTop: {
+				type: Number || String,
+				default: ''
+			}
+		},
+		methods:{}
+	}
+</script>
+
+<style>
+</style>
---
+++ weixin_uniapp/onekit/ui/movable-area/movable-area.vue
@@ -0,0 +1,20 @@
+<template>
+	<movable-area :scale-area="scaleArea">
+		<slot></slot>
+	</movable-area>
+</template>
+
+<script>
+	export default {
+		props: {
+			scaleArea: {
+				type: Boolean,
+				default: false
+			}
+		},
+		methods:{}
+	}
+</script>
+
+<style>
+</style>
