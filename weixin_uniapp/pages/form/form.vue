e977b176c47905ed35aa2c34080cb051e3059b19
gongyuan2 <939014677@qq.com>
2020-08-11 14:58:19 +0800

    from

--- weixin_uniapp/pages/form/form.vue
+++ weixin_uniapp/pages/form/form.vue
@@ -1,4 +1,5 @@
 <template>
+<<<<<<< HEAD
 	<view>
 		<page-head title="form"></page-head>
 		<view class="uni-padding-wrap uni-common-mt">
@@ -46,8 +47,59 @@
 			</form>
 		</view>
 	</view>
+=======
+	<view class="container">
+	  <view class="page-body">
+	    <onekit-form catchsubmit="formSubmit" catchreset="formReset">
+	      <view class="page-section page-section-gap">
+	        <view class="page-section-title">switch</view>
+	        <switch name="switch"/>
+	      </view>
+	
+	      <view class="page-section page-section-gap">
+	        <view class="page-section-title">radio</view>
+	        <radio-group name="radio">
+	          <label><radio value="radio1"/>选项一</label>
+	          <label><radio value="radio2"/>选项二</label>
+	        </radio-group>
+	      </view>
+	
+	      <view class="page-section page-section-gap">
+	        <view class="page-section-title">checkbox</view>
+	        <checkbox-group name="checkbox">
+	          <label><checkbox value="checkbox1"/>选项一</label>
+	          <label><checkbox value="checkbox2"/>选项二</label>
+	        </checkbox-group>
+	      </view>
+	
+	      <view class="page-section page-section-gap">
+	        <view class="page-section-title">slider</view>
+	        <slider value="50" name="slider" show-value ></slider>
+	      </view>
+	
+	      <view class="page-section">
+	        <view class="page-section-title">input</view>
+	        <view class="weui-cells weui-cells_after-title">
+	          <view class="weui-cell weui-cell_input">
+	            <view class="weui-cell__bd" style="margin: 30rpx 0" >
+	              <input class="weui-input" name="input" placeholder="这是一个输入框" v-model="testValue" />
+	            </view>
+	          </view>
+	        </view>
+	      </view>
+	
+	      <view class="btn-area">
+			<button style="margin: 30rpx 0" type="primary" formType="submit" @click="confirm">Submit</button>
+	        <button style="margin: 30rpx 0" formType="reset">Reset</button>
+	      </view>
+	    </onekit-form>
+	  </view>
+	</view>
+	</template>  
+>>>>>>> b268ba472ffc9fc8ceb8cc5d0bb997ca1de2ccee
 </template>
 <script>
+<<<<<<< HEAD
 	var  graceChecker = require("../../common/graceChecker.js");
 	export default {
 		data() {
@@ -83,4 +135,57 @@
 	.uni-form-item .title {
 		padding: 20rpx 0;
 	}
+=======
+	export default {
+		data() {
+			return {
+				pickerHidden: true,
+				chosen: '',
+				testValue: ''
+			}
+		},
+		methods: {
+			pickerConfirm(e) {
+			  this.setData({
+			    pickerHidden: true
+			  })
+			  this.setData({
+			    chosen: e.detail.value
+			  })
+			},
+				
+			pickerCancel() {
+			  this.setData({
+			    pickerHidden: true
+			  })
+			},
+				
+			pickerShow() {
+			  this.setData({
+			    pickerHidden: false
+			  })
+			},
+				
+			formSubmit(e) {
+			  console.log('form发生了submit事件，携带数据为：', e.detail.value)
+			},
+				
+			formReset(e) {
+			  console.log('form发生了reset事件，携带数据为：', e.detail.value)
+			  this.setData({
+			    chosen: ''
+			  })
+			},
+			confirm() {
+				let data = this.testValue;
+			    console.log(JSON.stringify(data))
+			},
+		}
+	}
+	
+</script>
+
+<style>
+
+>>>>>>> b268ba472ffc9fc8ceb8cc5d0bb997ca1de2ccee
 </style>
