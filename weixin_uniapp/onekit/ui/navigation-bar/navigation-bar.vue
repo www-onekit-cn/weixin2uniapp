339a296c5d7b3767316f4020f264e5843c0e89c8
liping <1850115720.com>
2020-08-13 11:04:05 +0800

    navigation-bar
    navigation-bar

---
+++ weixin_uniapp/onekit/ui/navigation-bar/navigation-bar.vue
@@ -0,0 +1,45 @@
+<template>
+	<navigation-bar
+		:title="title"
+		:loading="loading"
+		:front-color="frontColor"
+		:background-color="backgroundColor"
+		:color-animation-duration="colorAnimationDuration"
+		:color-animation-timing-func="colorAnimationTimingFunc"
+	></navigation-bar>
+</template>
+
+<script>
+	export default {
+		props:{
+			title:{
+				type:String,
+				default:""
+			},
+			loading:{
+				type:Boolean,
+				default:false
+			},
+			frontColor:{
+				type:String,
+				default:""
+			},
+			backgroundColor:{
+				type:String,
+				default:""
+			},
+			colorAnimationDuration:{
+				type:Number,
+				default:0
+			},
+			colorAnimationTimingFunc:{
+				type:String,
+				default:"linear"
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
