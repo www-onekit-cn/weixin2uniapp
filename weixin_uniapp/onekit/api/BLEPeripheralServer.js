b2b0d487d6f68f5abc7d1cad9951521b69001acb
onekit-qing <64195283+onekit-qing@users.noreply.github.com>
2020-08-17 18:02:28 +0800

    .

---
+++ weixin_uniapp/onekit/api/BLEPeripheralServer.js
@@ -0,0 +1,20 @@
+export default class BLEPeripheralServer {
+	constructor(uniBLEPeripheralServer) {
+		this.uniBLEPeripheralServer = uniBLEPeripheralServer
+	}
+	addService(object) {
+		var service
+		return this.addService(object)
+	}
+	close(object) {
+		return this.close(object)
+	}
+	offCharacteristicReadRequest(callback) {}
+	offCharacteristicWriteRequest(callback) {}
+	onCharacteristicReadRequest(callback) {}
+	onCharacteristicWriteRequest(callback) {}
+	removeService(object) {}
+	startAdvertising(object) {}
+	stopAdvertising(object) {}
+	writeCharacteristicValue(object) {}
+}
--- weixin_uniapp/onekit/api/NFCAdapter.js
+++ weixin_uniapp/onekit/api/NFCAdapter.js
@@ -2,4 +2,28 @@ export default class NFCAdapter {
 	constructor(uniNFCAdapter) {
 		this.uniNFCAdapter = uniNFCAdapter;
 	}
+	ndef(){
+		var 
+	}
+	nfcA(){
+		
+	}
+	nfcB(){
+		
+	}
+	isoDep(){
+		
+	}
+	nfcF(){
+		
+	}
+	nfcV(){
+		
+	}
+	mifareClassic(){
+		
+	}
+	mifareUltralight(){
+		
+	}
 }
