c8d74c1697d77cbcd4bb2ffe94e6849cbdb437d0
WangYeWei <826036140@qq.com>
2020-12-24 16:38:59 +0800

    .

--- weixin2uniapp/serverless/Collection.js
+++ weixin2uniapp/serverless/Collection.js
@@ -1,32 +1,82 @@
-export default class Collection{
-  constructor(db,name){
+export default class Collection {
+  constructor(db, name) {
     this.THIS = db.THIS.collection(name)
   }
-  get(wx_object){
-    let wx_success,wx_fail,wx_complete
-    if(wx_object){
-       wx_success = wx_object.success
-       wx_fail = wx_object.fail
-       wx_complete = wx_object.complete
+
+  get(wx_object) {
+    let wx_success, wx_fail, wx_complete
+    if (wx_object) {
+      wx_success = wx_object.success
+      wx_fail = wx_object.fail
+      wx_complete = wx_object.complete
     }
     wx_object = null
     //////////////////////
-    return new Promise((wx_resolve,wx_reject)=>{
-      this.THIS.get().then(({result})=>{
+    return new Promise((wx_resolve, wx_reject) => {
+      this.THIS.get().then(({
+        result
+      }) => {
         const wx_res = {
           errMsg: "collection.get:ok",
-          data:result.data
+          data: result.data
         }
         wx_resolve(wx_res)
-        if(wx_success){ wx_success(wx_res) }
-        if(wx_complete){ wx_complete(wx_res) }
-      }).catch((uni_err)=>{
+        if (wx_success) {
+          wx_success(wx_res)
+        }
+        if (wx_complete) {
+          wx_complete(wx_res)
+        }
+      }).catch((uni_err) => {
         const wx_err = {
-          errMsg:uni_err
+          errMsg: uni_err
         }
         wx_reject(wx_err)
-        if(wx_fail){ wx_fail(wx_err) }
-        if(wx_complete){ wx_complete(wx_err) }
+        if (wx_fail) {
+          wx_fail(wx_err)
+        }
+        if (wx_complete) {
+          wx_complete(wx_err)
+        }
+      })
+    })
+  }
+
+  add(wx_object) {
+    let wx_data, wx_success, wx_fail, wx_complete
+    wx_data = wx_object.data
+    if (wx_object.success || wx_object.fail || wx_object.complete) {
+      wx_success = wx_object.success
+      wx_fail = wx_object.fail
+      wx_complete = wx_object.complete
+    }
+    wx_object = null
+    return new Promise((wx_resolve, wx_reject) => {
+      this.THIS.add(wx_data).then(({
+        result
+      }) => {
+        const wx_res = {
+          _id: result.requestId,
+          errMsg: 'collection.add:ok'
+        }
+        wx_resolve(wx_res)
+        if (wx_success) {
+          wx_success(wx_res)
+        }
+        if (wx_complete) {
+          wx_complete(wx_res)
+        }
+      }).catch(uni_err => {
+        const wx_err = {
+          errrMsg: uni_err
+        }
+        wx_reject(wx_err)
+        if (wx_fail) {
+          wx_fail(wx_err)
+        }
+        if (wx_complete) {
+          wx_complete(wx_err)
+        }
       })
     })
   }
--- weixin2uniapp/serverless/Database.js
+++ weixin2uniapp/serverless/Database.js
@@ -1,13 +1,3 @@
-/*
- * @Autor: YeWei Wang
- * @Date: 2020-12-24 13:47:23
- * @WeChat: wj826036
- * @Motto: 求知若渴，虚心若愚
- * @Description: 
- * @LastEditTime: 2020-12-24 14:12:54
- * @Version: 1.0
- * @FilePath: \weixin2uniapp\weixin2uniapp\serverless\Database.js
- */
 import Collection from './Collection'
 export default class Database{
   constructor(){
