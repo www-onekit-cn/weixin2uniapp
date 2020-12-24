2c36783007d9fea72015d9295d3b4fc1bacd0fe1
WangYeWei <826036140@qq.com>
2020-12-24 17:31:13 +0800

    .

--- weixin2uniapp/serverless/Collection.js
+++ weixin2uniapp/serverless/Collection.js
@@ -87,6 +87,44 @@ export default class Collection {
   }
 
   remove(wx_object) {
+    let wx_success, wx_fail, wx_complete
+    if (wx_object) {
+      wx_success = wx_object.success
+      wx_fail = wx_object.fail
+      wx_complete = wx_object.complete
+    }
+    wx_object = null
+    //////////////////////
+    return new Promise((wx_resolve, wx_reject) => {
+      this.THIS.remove().then(({
+        result
+      }) => {
+        const wx_res = {
+          errMsg: "collection.remove:ok",
+          stats: {
+            removed: 0
+          }
+        }
+        wx_resolve(wx_res)
+        if (wx_success) {
+          wx_success(wx_res)
+        }
+        if (wx_complete) {
+          wx_complete(wx_res)
+        }
+      }).catch((uni_err) => {
+        const wx_err = {
+          errMsg: uni_err
+        }
+        wx_reject(wx_err)
+        if (wx_fail) {
+          wx_fail(wx_err)
+        }
+        if (wx_complete) {
+          wx_complete(wx_err)
+        }
+      })
+    })
   }
-  
+
 }
