612fbda0eb8004fd082192f8767821a2f70bfb84
WangYeWei <826036140@qq.com>
2020-12-28 17:45:18 +0800

    .

--- weixin2uniapp/serverless/wx.cloud.js
+++ weixin2uniapp/serverless/wx.cloud.js
@@ -1,6 +1,6 @@
 import Database from './Database'
 export default class wx_cloud {
-  static init(wx_options){
+  static init(wx_options) {
     const env = wx_options.env
     const traceUser = wx_options.traceUser || false
     wx_options = null
@@ -9,13 +9,57 @@ export default class wx_cloud {
     this.traceUser = traceUser
   }
 
-  static database () {
+  static database() {
     let wx_database = this.wx_database
-    if(!wx_database){
+    if (!wx_database) {
       wx_database = new Database(uniCloud.database())
       this.wx_database = wx_database
     }
     return wx_database
   }
 
+  static getTempFileURL(wx_object) {
+    let wx_success, wx_fail, wx_complete
+    const fileList = wx_object.fileList
+    if (wx_object.success || wx_object.fail || wx_object.complete) {
+      wx_success = wx_object.success
+      wx_fail = wx_object.fail
+      wx_complete = wx_object.complete
+    }
+    wx_object = null
+    return new Promise((wx_resolve, wx_reject) => {
+      uniCloud.getTempFileURL({
+        fileList,
+      }).then(res => {
+        const resu = {
+          errMsg: 'cloud.getTempFileURL:ok',
+          fileList: res.fileList.map(res => {
+            const obj = {
+              errMsg: 'OK',
+              fileID: res.fileID,
+              maxAge: undefined,
+              status: 0,
+              tempFileURL: res.tempFileURL
+            }
+            return obj
+          })
+        }
+        wx_resolve(resu)
+        if(wx_success) {
+          wx_success(resu)
+        }
+        if(wx_complete) {
+          wx_complete(resu)
+        }
+      }).catch(err => {
+        wx_reject(err)
+        if(wx_fail) {
+          wx_fail(err)
+        }
+        if(wx_complete) {
+          wx_complete(err)
+        }
+      })
+    })
+  }
 }
