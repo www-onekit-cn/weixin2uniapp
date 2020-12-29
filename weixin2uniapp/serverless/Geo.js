25e7057054a7e775529a5ecfebef0ee0b0617f7c
WangYeWei <826036140@qq.com>
2020-12-29 18:03:20 +0800

    .

--- weixin2uniapp/serverless/Geo.js
+++ weixin2uniapp/serverless/Geo.js
@@ -4,7 +4,22 @@ export default class Geo {
   }
 
   Point(longitude, latitude) {
-    // return new Geo(this.THIS.Geo(longitude, latitude))
     return new this.THIS.Point(longitude, latitude)
+  }
+
+  LineString(wx_points) {
+    const res = new this.THIS.LineString(wx_points)
+    const uni_points = res.param[0].map(res => res.param)
+    let arrRes = {}
+    let longtitude = []
+    let latitude = []
+
+
+    for(let i in uni_points) {
+      longtitude.push(uni_points[i][i-1])
+      latitude.push(uni_points[i][i+1])
+    }
+
+    return new this.THIS.LineString(wx_points)
   }
 }
