9736a0dfc363b3f5a7511d7382d5f902535d4894
WangYeWei <826036140@qq.com>
2020-12-30 14:06:46 +0800

    .

--- weixin2uniapp/serverless/Geo.js
+++ weixin2uniapp/serverless/Geo.js
@@ -1,4 +1,6 @@
+/** 数据库地理位置结构集 */
 export default class Geo {
+
   constructor(uni_database) {
     this.THIS = uni_database
   }
@@ -34,4 +36,42 @@ export default class Geo {
     }
     return result
   }
+
+  MultiPoint(points) {
+    const res = new this.THIS.MultiPoint(points)
+    const uni_params = res.param[0].map(res => res.param)
+    let resultObj = {
+      points: []
+    }
+    for (let key of uni_params) {
+      const res = {
+        st: {
+          latitude: key[1],
+          longtitud: key[0],
+          _internalType: Symbol('GEO_POINT')
+        }
+      }
+      resultObj.points.push(res)
+    }
+    resultObj._internalType = Symbol('Geo_POINT')
+    return resultObj
+  }
+
+  MultiLineString(linestring) {
+    const res = new this.THIS.MultiLineString(linestring)
+    const result = {
+      lines: res.param,
+      _internalType: Symbol('GEO_MULTILINESTRING')
+    }
+    return result
+  }
+  
+  GeoMultiPolygon(linestring) {
+    const res = new this.THIS.GeoMultiPolygon(linestring)
+    const result = {
+      lines: res.param
+    }
+    return result
+  }
+
 }
