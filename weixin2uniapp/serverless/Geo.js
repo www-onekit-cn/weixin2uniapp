9c8efec13f9c4411bec55c243fd5c6401940a34d
WangYeWei <826036140@qq.com>
2020-12-30 12:02:15 +0800

    .

--- weixin2uniapp/serverless/Geo.js
+++ weixin2uniapp/serverless/Geo.js
@@ -10,7 +10,9 @@ export default class Geo {
   LineString(wx_points) {
     const res = new this.THIS.LineString(wx_points)
     const uni_params = res.param[0].map(res => res.param)
-    let resultObj = {points: []}
+    let resultObj = {
+      points: []
+    }
     for (let key of uni_params) {
       const res = {
         st: {
@@ -23,5 +25,13 @@ export default class Geo {
     }
     resultObj._internalType = Symbol('Geo_POINT')
     return resultObj
+  }
+
+  Polygon(linestring) {
+    const res = new this.THIS.Polygon(linestring)
+    const result = {
+      lines: res.param
+    }
+    return result
   }
 }
