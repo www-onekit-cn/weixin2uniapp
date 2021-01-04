56d4da7d1321c598bacc66b322f5637214d7c52a
15802303637 <>
2021-01-04 13:28:59 +0800

    .

--- weixin2uniapp/OnekitPage.js
+++ weixin2uniapp/OnekitPage.js
@@ -5,6 +5,27 @@ export default function(PAGE_JSON, wx_object) {
     },
     onLoad(query) {
       this.data = this.$data;
+      if(wx_object){
+        for (let key of Object.keys(wx_object)) {
+          let obj = wx_object[key];
+          switch (key) {
+            case "data":
+            case "onLoad":
+            case "onUnload":
+            case "onReady":
+            case "onShow":
+            case "onHide":
+              break;
+            default:
+              if(typeof(obj)==='function'){
+                uni_object.methods[key] = obj;
+              }else{
+                this.data[key] = obj
+              }
+              break;
+          }
+        }
+      }
       if (wx_object && wx_object["onLoad"]) {
         wx_object["onLoad"].call(this,query );
       }
@@ -48,25 +69,6 @@ export default function(PAGE_JSON, wx_object) {
       uni_object.data = () => {
         return wx_object.data;
       };
-    }
-    for (let key of Object.keys(wx_object)) {
-      let obj = wx_object[key];
-      switch (key) {
-        case "data":
-        case "onLoad":
-        case "onUnload":
-        case "onReady":
-        case "onShow":
-        case "onHide":
-          break;
-        default:
-          if(typeof(obj)==='function'){
-            uni_object.methods[key] = obj;
-          }else{
-            this.data[key] = obj
-          }
-          break;
-      }
     }
   }
   return uni_object;
