b4b1509ab7867d6f879e0adc629d62491b32494e
15802303637 <>
2021-01-07 14:45:23 +0800

    .

--- weixin2uniapp/OnekitComponent.js
+++ weixin2uniapp/OnekitComponent.js
@@ -1,26 +1,7 @@
 export default function(UC_JSON, wx_object) {
   let uni_object = {
-    onLoad(query) {
+    attached() {
       this.data = this.$data;
-      if(wx_object){
-        for (let key of Object.keys(wx_object)) {
-          let obj = wx_object[key];
-          switch (key) {
-            case "data":
-            case "onLoad":
-            case "onUnload":
-            case "onReady":
-            case "onShow":
-            case "onHide":
-              break;
-            default:
-              if(typeof(obj)!=='function'){
-                this.data[key] = obj
-              }
-              break;
-          }
-        }
-      }
       if(wx_object && wx_object.pageLifetimes && wx_object.pageLifetimes.attached){
         wx_object.pageLifetimes.attached.call(this);
       }
@@ -28,7 +9,7 @@ export default function(UC_JSON, wx_object) {
         wx_object["attached"].call(this );
       }
     },
-    onUnload() {
+    detached() {
       if(wx_object && wx_object.pageLifetimes && wx_object.pageLifetimes.attadetachedched){
         wx_object.pageLifetimes.detached.call(this);
       }
@@ -55,22 +36,9 @@ export default function(UC_JSON, wx_object) {
       uni_object.data = () => {
         return wx_object.data;
       };
-      for (let key of Object.keys(wx_object)) {
-        let obj = wx_object[key];
-        switch (key) {
-          case "data":
-          case "onLoad":
-          case "onUnload":
-          case "onReady":
-          case "onShow":
-          case "onHide":
-            break;
-          default:
-            if(typeof(obj)==='function'){
-              uni_object.methods[key] = obj;
-            }
-            break;
-        }
+      for (let key of Object.keys(wx_object.methods)) {
+        let func = wx_object.methods[key];
+        uni_object.methods[key] = func;
       }
     }
   }
